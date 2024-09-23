b7083f27ea56f13fd8b65457694942343aadb87f
NyamburaKariuki <prettimercii@gmail.cm>
2024-09-23 18:06:16 +0300

    basic set up

---
+++ 0x00-Webpack/task_0/dist/index.js
@@ -0,0 +1,10 @@
+// Importing jQuery
+import $ from 'jquery';
+
+// Adding three different paragraphs to the body
+$(document).ready(function () {
+  const paragraphs = ['<p>This is the first paragraph.</p>', '<p>This is the second paragraph.</p>', '<p>This is the third paragraph.</p>'];
+  paragraphs.forEach(paragraph => {
+    $('body').append(paragraph);
+  });
+});
